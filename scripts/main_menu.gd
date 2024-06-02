2c6612bba8feae5c81d6dd4f24c527e3b724255d
NathanBerglas <nathan.berglas@gmail.com>
2024-06-02 12:04:57 -0400

    Added script to level one

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,27 +1,19 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/test.tscn") # Need to change to level one (or whatever current level should be)
-
-
-func _on_how_2_play_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/test.tscn") # Need to change to instructions scene
-
+	get_tree().change_scene_to_file("res://scenes/Levels/level_one.tscn") # Need to change to level one (or whatever current level should be)
 
 func _on_options_button_pressed():
+	print("uh oh")
 	get_tree().change_scene_to_file("res://scenes/test.tscn") # Need to change to options menu
-
 
 func _on_quit_button_pressed():
 	get_tree().quit()
