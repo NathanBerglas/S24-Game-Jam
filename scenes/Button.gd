0dfc46ab9c076394e157c522175529df4506ca77
NathanBerglas <nathan.berglas@gmail.com>
2024-06-02 15:33:49 -0400

    Actualy done, maybe...

--- scenes/Button.gd
+++ scenes/Button.gd
@@ -1,5 +1,7 @@
 extends Button
 
+func _ready():
+	get_window().content_scale_size = Vector2i(1920, 1080)
 
 func _on_pressed():
 	get_tree().change_scene_to_file("res://scenes/tests/house_test.tscn")
